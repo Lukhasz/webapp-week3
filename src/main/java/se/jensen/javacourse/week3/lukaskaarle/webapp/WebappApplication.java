a960afe2099661000d02e7fe59e279f3a1057866
Lukhasz <lukaskaarle@gmail.com>
2022-12-01 16:18:50 +0100

    initial commit

---
+++ src/main/java/se/jensen/javacourse/week3/lukaskaarle/webapp/WebappApplication.java
@@ -0,0 +1,15 @@
+package se.jensen.javacourse.week3.lukaskaarle.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/se/jensen/javacourse/week3/lukaskaarle/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.javacourse.week3.lukaskaarle.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
