c3b23398c6763b8e06d9e7446f0736dc169daa26
Lukhasz <lukaskaarle@gmail.com>
2022-12-01 17:54:24 +0100

    added sqare root function on /square-number/{nr}

--- src/main/java/se/jensen/javacourse/week3/lukaskaarle/webapp/WebApplication.java
+++ src/main/java/se/jensen/javacourse/week3/lukaskaarle/webapp/WebApplication.java
@@ -3,11 +3,14 @@ package se.jensen.javacourse.week3.lukaskaarle.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class WebApplication {
+
+	private double squareRoot;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
@@ -30,6 +33,11 @@ public class WebApplication {
 	@GetMapping("/author/name")
 	public String author() {
 		return "Lukas";
+	}
+
+	@GetMapping("/square-number/{nr}")
+	public String read(@PathVariable(name = "nr") double squareRoot) {
+		return "square root of " + squareRoot + " is " + Math.sqrt(squareRoot);
 	}
 
 
